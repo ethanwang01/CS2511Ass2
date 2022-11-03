d35bd09c0662aff2ad4252948aa54381a9aacfdb
Ethan Wang <z5317971@ad.unsw.edu.au>
2022-11-04 02:24:07 +1100

    Sunstone functionality no tests

---
+++ src/main/java/dungeonmania/entities/collectables/Sunstone.java
@@ -0,0 +1,12 @@
+package dungeonmania.entities.collectables;
+
+import dungeonmania.entities.CollectableEntity;
+import dungeonmania.entities.inventory.InventoryItem;
+import dungeonmania.util.Position;
+
+public class Sunstone extends CollectableEntity implements InventoryItem {
+        public Sunstone(Position position) {
+                super(position);
+        }
+        
+}
